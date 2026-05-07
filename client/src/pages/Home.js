6e4da8651a2ecded7ec98e46d46d39bda103a6d4
Saitej Katturi <143821129+saitej2327@users.noreply.github.com>
2026-05-07 13:14:45 -0400

    added food homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,39 @@
 function Home() {
   const foods = [
-    { id: 1, name: "Burger", price: "$10" },
-    { id: 2, name: "Pizza", price: "$15" },
-    { id: 3, name: "Pasta", price: "$12" }
+    {
+      id: 1,
+      name: "Burger",
+      price: "$10"
+    },
+    {
+      id: 2,
+      name: "Pizza",
+      price: "$15"
+    },
+    {
+      id: 3,
+      name: "Pasta",
+      price: "$12"
+    }
   ];
 
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Food Ordering App 🍔</h1>
 
       {foods.map((food) => (
-        <div key={food.id}>
+        <div
+          key={food.id}
+          style={{
+            border: "1px solid gray",
+            padding: "10px",
+            marginBottom: "10px"
+          }}
+        >
           <h2>{food.name}</h2>
           <p>{food.price}</p>
+
           <button>Order Now</button>
-          <hr />
         </div>
       ))}
     </div>
